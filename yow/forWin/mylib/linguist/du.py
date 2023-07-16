c136dca74a768751670ad36dda584ffa7f0d694d
tetraloba <four.leaves.siva7342@gmail.com>
2023-07-16 15:44:56 +0900

    replace_subprocess_with_standardFunction_in_du.py

--- yow/forWin/mylib/linguist/du.py
+++ yow/forWin/mylib/linguist/du.py
@@ -1,33 +1,22 @@
-import subprocess
-from os import chdir, getcwd
+from os import path
+from glob import glob
 
 from .config import settings
-from .suffix import func
 
-def get_sizes_per_lang(path: str) -> dict:
+def get_sizes_per_lang(dir_path: str) -> dict:
 
-    # カレントディレクトリの操作と表示
-    # print('now directory is ' + getcwd()) # debug
-    # chdir(path)
-    # print('now directory is ' + getcwd()) # debug
-
-    # 各言語の容量を計測 (find -name "*.EXT" | xargs du -c | tail -n 1 | cut -f1)
+    # 各言語の容量を計測 (find -name "*.EXT" | xargs du -c | tail -n 1 | cut -f1 に相当)
     sizes = dict() # 言語:容量
     for lang, prop in settings['langs'].items():
         for ext in prop[1]:
-            cp = subprocess.run(['wsl', '--', 'find', path, '-name', f'*.{ext}'], capture_output=True)
-            # print(cp.stdout) # debug
-            if cp.stdout == '': # 該当するファイルが見つからない場合は容量0
-                size = 0
+            # print(path.join(dir_path, '**', f'*.{ext}')) # debug
+            file_list = glob(path.join(dir_path, '**', f'*.{ext}'), recursive=True)
+            if file_list.__len__() == 0:
                 break
-            cp = subprocess.run(['wsl', '--', 'xargs', 'du', '-ch', '--apparent-size'], input=cp.stdout, capture_output=True)
-            # print(cp.stdout) # debug
-            cp = subprocess.run(['wsl', '--', 'tail', '-n 1'], input=cp.stdout, capture_output=True) # 最終行を取り出す
-            # print(cp.stdout) # debug
-            cp = subprocess.run(['wsl', '--', 'cut', '-f1'], input=cp.stdout, capture_output=True)
-            # print(lang, cp.stdout.decode()) # debug
-            size = func(cp.stdout.decode().strip()) # コマンドの結果(接尾辞付きの文字列)を 接尾辞なしのfloat型に変換
+            # print(file_list) # debug
+            size = sum(path.getsize(file) for file in file_list)
             sizes[lang] = size + sizes.get(lang, 0)
+
     # for key, value in sizes.items():
     #     print(key, value) # debug
     return sizes
