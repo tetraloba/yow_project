33990a12c91b43eaa5c8d866693d8eb73998363e
tetraloba <four.leaves.siva7342@gmail.com>
2023-07-18 05:22:38 +0900

    simplify_git.py

--- yow/forWin/mylib/heatmap/git.py
+++ yow/forWin/mylib/heatmap/git.py
@@ -1,26 +1,18 @@
 import subprocess
-from os import chdir, getcwd
 from datetime import datetime
 
-from .config import settings
-
 def get_commits_per_date(path):
-    # path = "~/projects/competitiveProgramming"
+    # path = "~/projects/competitiveProgramming" # debug
 
-    # git log コマンドの実行 と 解析用コマンドの実行
-    # cp = subprocess.run(["wsl", "--", "git", "-C", path, "log"], capture_output=True, text=True, shell=True, encoding='utf-8')
-    cp = subprocess.run(["wsl", "--", "git", "-C", path, "log"], shell=True, capture_output=True)
-    # print('stdout is ', cp.stdout) # debug
-    # print(cp) # debug
-    line_date = [line.decode() for line in cp.stdout.split(b'\n') if b'Date: ' in line] # 'Date: 'の含まれる行を抽出(grep的な)
+    # git log コマンドの実行
+    res = subprocess.run(["wsl", "--", "git", "-C", path, "log", "--pretty=format:'%ai'"], shell=True, capture_output=True, text=True).stdout
+
     # 年/月/日 の列を抽出して各日の出現数をカウント(awk + uniq的な)
     dates = dict()
-    for line in line_date:
-        l = line.split()
-        date = datetime.strptime(f'{l[5]}/{l[2]}/{l[3]}', '%Y/%b/%d').date()
+    for line in res.split('\n'):
+        date = datetime.strptime(line.split()[0], '%Y-%m-%d').date()
         dates[date] = dates.get(date, 0) + 1
     # for date, cnt in dates.items():
-    #     print(date, cnt)
+    #     print(date, cnt) # debug
 
-    # return [s.strip() for s in cp.stdout.split('\n')]
     return dates
