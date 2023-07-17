e92bcf14500358eb6e2a21cdcc247bec8f861783
tetraloba <four.leaves.siva7342@gmail.com>
2023-07-18 05:22:04 +0900

    adjust_heatmap_size

--- yow/forWin/mylib/integrate/config.py
+++ yow/forWin/mylib/integrate/config.py
@@ -1,5 +1,5 @@
 svg_files = {
-    r'.\tmp\heatmap.svg':{'width':1920, 'height':310, 'x':80, 'y':60},
+    r'.\tmp\heatmap.svg':{'width':1840, 'height':310, 'x':80, 'y':60},
     r'.\tmp\linguist.svg':{'width':1920, 'height':220, 'x':0, 'y':390},
     r'.\tmp\calendar.svg':{'width':1152, 'height':648, 'x':768, 'y':432},
     r'.\tmp\abstinence.svg':{'width':1800, 'height':75, 'x':0, 'y':850},
